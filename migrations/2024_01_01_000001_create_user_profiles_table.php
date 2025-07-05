fac6b8722590941d3f5428f61488e372b29b0d30
Jmizutani <mizutani513@gmail.com>
2025-07-05 15:47:13 +0900

    fix

--- migrations/2024_01_01_000001_create_user_profiles_table.php
+++ migrations/2024_01_01_000001_create_user_profiles_table.php
@@ -6,9 +6,6 @@ use Illuminate\Database\Schema\Blueprint;
 return Migration::createTable('user_profiles', function (Blueprint $table) {
     $table->increments('id');
     $table->integer('user_id')->unsigned();
-    $table->text('introduction')->nullable();
-    $table->text('childcare_situation')->nullable();
-    $table->text('care_situation')->nullable();
     $table->string('facebook_url')->nullable();
     $table->string('x_url')->nullable();
     $table->string('instagram_url')->nullable();
